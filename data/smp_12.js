3bfec557e0b517e9fe819c3fc865a5d36b6d0305
dessydamitri <damitridessy169@gmail.com>
2025-01-19 16:05:29 +0700

    Add files via upload

---
+++ data/smp_12.js
@@ -0,0 +1,1 @@
+var json_smp_12 = {"type":"FeatureCollection","name":"smp_12","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":null,"nama":"SMP N 4 BENAI","Gambar":null},"geometry":{"type":"Point","coordinates":[101.627562922511871,-0.495735534101426]}}]}
